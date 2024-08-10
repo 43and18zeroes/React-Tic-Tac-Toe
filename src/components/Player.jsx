28d4c2efe6192fb7b8c22604aed65247156fe4f8
Christoph <c.wagner.germany@gmail.com>
2024-08-10 10:23:31 +0200

    state4

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -7,11 +7,16 @@ export default function Player({ name, symbol }) {
     setIsEditing(true);
   }
 
+  let playerName = <span className="player-name">{name}</span>;
+
+  if (isEditing) {
+    playerName = <input type="text" required />;
+  }
+
   return (
     <li>
       <span className="player">
-        {!isEditing && <span className="player-name">{name}</span>}
-        {isEditing && <input />}
+        {playerName}
         <span className="player-symbol">{symbol}</span>
       </span>
       <button onClick={handleEditClick}>Edit</button>
