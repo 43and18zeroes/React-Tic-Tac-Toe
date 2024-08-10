b9a17124e386bbe328da82bd778d7411f134de1d
Christoph <c.wagner.germany@gmail.com>
2024-08-10 10:14:43 +0200

    state2

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,13 +1,20 @@
-import { useState } from "react";
+import { useState } from 'react';
 
 export default function Player({ name, symbol }) {
+  const [isEditing, setIsEditing] = useState();
+
+  function editName() {
+    setIsEditing(true);
+  }
+
   return (
     <li>
       <span className="player">
-        <span className="player-name">{name}</span>
+        {!isEditing && <span className="player-name">{name}</span>}
+        {isEditing && <input>{name}</input>}
         <span className="player-symbol">{symbol}</span>
       </span>
-      <button>Edit</button>
+      <button onClick={editName}>Edit</button>
     </li>
   );
 }
