4bc9d7616e70ed671ecea704b5c92c8efcd22a34
Christoph <c.wagner.germany@gmail.com>
2024-08-10 17:23:09 +0200

    state17

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -8,6 +8,14 @@ const initialGameBoard = [
 
 export default function GameBoard() {
   const [gameBoard, setGameBoard] = useState(initialGameBoard);
+
+  function handleSelectSquare(rowIndex, colIndex) {
+    setGameBoard((prevGameBoard) => {
+      prevGameBoard[rowIndex][colIndex] = 'X';
+      return prevGameBoard;
+    });
+  }
+
   return (
     <ol id="game-board">
       {initialGameBoard.map((row, rowIndex) => (
